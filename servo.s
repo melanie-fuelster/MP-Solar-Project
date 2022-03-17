05945503dc5e89e32d90f12f3592abd25c32d121
mf919 <mf919@skbl-422-005.ph.ic.ac.uk>
2022-03-17 11:58:16 +0000

    implemented servo movement based on solar panel readings :D

--- servo.s
+++ servo.s
@@ -1,8 +1,8 @@
 #include <xc.inc>
 
-global  Servo_Setup, Create_Pulse, Create_small_Pulse
+global  Servo_Setup, Create_Pulse, Create_small_Pulse, move_servo
     
-extrn   delay_x4us, delay_100us, delay_ms,delay_12us
+extrn   delay_x4us, delay_100us, delay_ms,delay_12us, sign
     
 psect udata_acs
 pulse_width:    ds 1 
@@ -15,25 +15,40 @@ Servo_Setup:
 	movwf	TRISJ, A
 	return
 
+move_servo:
+	btfsc	sign, 0, A
+	call	move_right
+	call	move_left
+	return
+
+move_right:
+	movlw	0x12		;1.6 ms pulse
+	call	Create_Pulse
+	return
+move_left:
+	movlw	0x0a		;1.3 ms pulse
+	call	Create_Pulse
+	return
+	
 Create_Pulse:
-    movwf   pulse_width, A
-    movlw   0x01	
-    movwf   PORTJ, A    ;start of pulse
-    movff   pulse_width, WREG, A
-    call    delay_100us ;define pulse length
-    movlw   0x00
-    movwf   PORTJ, A    ;end of pulse
+	movwf   pulse_width, A
+	movlw   0x01	
+	movwf   PORTJ, A    ;start of pulse
+	movff   pulse_width, WREG, A
+	call    delay_100us ;define pulse length
+	movlw   0x00
+	movwf   PORTJ, A    ;end of pulse
 return
 
 Create_small_Pulse:
-    movwf   pulse_width, A
-    movlw   0x01	
-    movwf   PORTJ, A    ;start of pulse
-    movff   pulse_width, WREG, A
-    call    delay_12us ;define pulse length
-    movlw   0x00
-    movwf   PORTJ, A    ;end of pulse
-return
+	movwf   pulse_width, A
+	movlw   0x01	
+	movwf   PORTJ, A    ;start of pulse
+	movff   pulse_width, WREG, A
+	call    delay_12us ;define pulse length
+	movlw   0x00
+	movwf   PORTJ, A    ;end of pulse
+	return
 	
 Test_Pulse:
 	movlw	0x01	
